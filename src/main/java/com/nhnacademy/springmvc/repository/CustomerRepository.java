023d05d69fa86d810c84d971423f6f159e4846c3
minsu <zmfjtnl1@naver.com>
2024-01-12 13:59:22 +0900

    Add Method repository package

--- src/main/java/com/nhnacademy/springmvc/repository/CustomerRepository.java
+++ src/main/java/com/nhnacademy/springmvc/repository/CustomerRepository.java
@@ -1,11 +1,12 @@
 package com.nhnacademy.springmvc.repository;
 
 import com.nhnacademy.springmvc.domain.Customer;
+import com.nhnacademy.springmvc.domain.Role;
 
 public interface CustomerRepository {
     boolean exists(String id);
 
-    Customer register(String id, String password);
+    Customer register(String id, String password, String name, Role role);
 
     Customer getCustomer(String id);
 
--- src/main/java/com/nhnacademy/springmvc/repository/CustomerRepositoryImpl.java
+++ src/main/java/com/nhnacademy/springmvc/repository/CustomerRepositoryImpl.java
@@ -1,20 +1,37 @@
 package com.nhnacademy.springmvc.repository;
 
 import com.nhnacademy.springmvc.domain.Customer;
+import com.nhnacademy.springmvc.domain.Role;
+import com.nhnacademy.springmvc.exception.CustomerAlreadyExistsException;
+import com.nhnacademy.springmvc.exception.CustomerNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomerRepositoryImpl implements CustomerRepository {
+
+    private Map<String, Customer> customerMap = new HashMap<>();
+
     @Override
     public boolean exists(String id) {
-        return false;
+        return customerMap.containsKey(id);
     }
 
     @Override
-    public Customer register(String id, String password) {
-        return null;
+    public Customer register(String id, String password, String name, Role role) {
+        if (exists(id)) {
+            throw new CustomerAlreadyExistsException();
+        }
+        Customer customer = Customer.create(id, password);
+        customer.setName(name);
+        customer.setRole(role);
+        return customer;
     }
 
     @Override
     public Customer getCustomer(String id) {
-        return null;
+        if (exists(id)) {
+            throw new CustomerNotFoundException();
+        }
+        return customerMap.get(id);
     }
 }
