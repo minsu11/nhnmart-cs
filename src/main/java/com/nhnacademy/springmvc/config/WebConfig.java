d40b0bf6f845f69b5ad449ff6de06c8211e65454
minsu <zmfjtnl1@naver.com>
2024-01-12 10:02:17 +0900

    Add annotation and setapplicationContext WebConfig.java

--- src/main/java/com/nhnacademy/springmvc/config/WebConfig.java
+++ src/main/java/com/nhnacademy/springmvc/config/WebConfig.java
@@ -1,9 +1,22 @@
 package com.nhnacademy.springmvc.config;
 
+import com.nhnacademy.springmvc.controller.BaseController;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-public class WebConfig {
+@ComponentScan(basePackageClasses = BaseController.class)
+public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
 }
