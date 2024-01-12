7be0343f7874d86af0b01ca66eaf9b89e02f88ab
minsu <zmfjtnl1@naver.com>
2024-01-12 14:22:12 +0900

    upload

--- src/main/java/com/nhnacademy/springmvc/controller/LoginController.java
+++ src/main/java/com/nhnacademy/springmvc/controller/LoginController.java
@@ -1,10 +1,12 @@
 package com.nhnacademy.springmvc.controller;
 
 import com.nhnacademy.springmvc.repository.CustomerRepository;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/cs")
@@ -15,13 +17,27 @@ public class LoginController {
         this.customerRepository = customerRepository;
     }
 
+
     @GetMapping("/login")
     public String login() {
         return "thymeleaf/loginForm";
     }
 
     @PostMapping("/login")
-    public String doLogin(){
+    public String doLogin(@RequestParam("id") String id,
+                          @RequestParam("password") String password,
+                          HttpServletRequest request,
+                          HttpServletResponse response,
+                          ModelMap modelMap){
+        if(customerRepository.matches(id,password)){
+            HttpSession session = request.getSession();
+            session.setAttribute("customerId",id);
+            session.setMaxInactiveInterval(1800);
+            modelMap.addAttribute("id", session);
+
+        }
+
+
         return
     }
 
