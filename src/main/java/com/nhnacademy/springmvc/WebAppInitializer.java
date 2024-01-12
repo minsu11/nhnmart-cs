96998fc15a97c6d42a0383a075987995f0f8bbef
minsu <zmfjtnl1@naver.com>
2024-01-12 09:57:28 +0900

    Add AbstractAnnotationConfigDispatcherServletInitialize extends WebAppInitializer.java

--- src/main/java/com/nhnacademy/springmvc/WebAppInitializer.java
+++ src/main/java/com/nhnacademy/springmvc/WebAppInitializer.java
@@ -1,6 +1,21 @@
 package com.nhnacademy.springmvc;
 
+import com.nhnacademy.springmvc.config.RootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {RootConfig.class};
+    }
 
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
