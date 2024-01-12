166580d0b16c23afa7e99f021bd6b8c46445fc5c
minsu <zmfjtnl1@naver.com>
2024-01-12 13:55:04 +0900

    Add Bean RootConfig.java

--- src/main/java/com/nhnacademy/springmvc/config/RootConfig.java
+++ src/main/java/com/nhnacademy/springmvc/config/RootConfig.java
@@ -1,6 +1,10 @@
 package com.nhnacademy.springmvc.config;
 
 import com.nhnacademy.springmvc.Base;
+import com.nhnacademy.springmvc.domain.Role;
+import com.nhnacademy.springmvc.repository.CustomerRepository;
+import com.nhnacademy.springmvc.repository.CustomerRepositoryImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
@@ -9,5 +13,13 @@ import org.springframework.stereotype.Controller;
 @ComponentScan(basePackageClasses = Base.class,
         excludeFilters = {@ComponentScan.Filter(Controller.class)})
 public class RootConfig {
+    @Bean
+    public CustomerRepository customerRepository() {
+        CustomerRepository customerRepository = new CustomerRepositoryImpl();
+        customerRepository.register("admin", "1234", "adnin", Role.ADMIN);
+
+        return customerRepository;
+
+    }
 
 }
