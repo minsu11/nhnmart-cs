7f023744ab9bb6843f6cc09e89a95530e8e38f55
minsu <zmfjtnl1@naver.com>
2024-01-12 10:04:56 +0900

    Override configureViewResolvers and Add ThymeleafViewResolver()

--- src/main/java/com/nhnacademy/springmvc/config/WebConfig.java
+++ src/main/java/com/nhnacademy/springmvc/config/WebConfig.java
@@ -4,10 +4,13 @@ import com.nhnacademy.springmvc.controller.BaseController;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @EnableWebMvc
 @Configuration
@@ -19,4 +22,16 @@ public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
     }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        WebMvcConfigurer.super.configureViewResolvers(registry);
+    }
+
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+    }
+    
+
 }
